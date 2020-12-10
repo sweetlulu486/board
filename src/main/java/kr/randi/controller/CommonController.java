f8b1b2dbba396b89b2837fc205e422e362ac897b
Randi <sweetlulu486@gmail.com>
2020-12-10 20:00:24 +0900

    로그인페이지 생성

---
+++ src/main/java/kr/randi/controller/CommonController.java
@@ -0,0 +1,44 @@
+package kr.randi.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import kr.randi.domain.BoardVO;
+import kr.randi.domain.Criteria;
+import kr.randi.domain.PageDTO;
+import kr.randi.service.BoardService;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/first/*")
+public class CommonController {
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		log.info("error: " + error);
+		log.info("logout: " + logout);
+
+		if (error != null) {
+			model.addAttribute("error", "Login Error Check Your Account");
+		}
+
+		if (logout != null) {
+			model.addAttribute("logout", "Logout!!");
+		}
+	}
+}
